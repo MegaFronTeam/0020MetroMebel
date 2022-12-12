d86f6b5a0696c1c96203341ca8c458d6808610eb
Janis <janis.paberzs18@gmail.com>
2022-12-12 14:42:20 +0200

    edit block with js

---
+++ public/js/content.js
@@ -0,0 +1,32 @@
+const contentsMetalFurniture = `
+  <!-- start sMetalFurniture-->
+  <section class="sMetalFurniture section" id="sMetalFurniture">
+    <div class="sMetalFurniture__container container"><img class="bg" src="img/sAdvantages-img-2.png" alt=""/>
+      <div class="sMetalFurniture__row row">
+        <div class="col-xl">
+          <div class="sMetalFurniture__left-side">
+            <div class="h1">металлическая мебель</div>
+            <h4>Комплексная установка под ключ для вашего бизнеса!</h4>
+            <ul> 
+              <li><span>Мы предлагаем </span> стеллажи и&nbsp;медицинскую мебель, шкафы для документов и&nbsp;одежды, сейфы и почтовые ящики, верстаки и&nbsp;другое оборудование. Практичные решения для производства, офиса и архива, мастерской и&nbsp;склада, гостиницы или хостела, больницы и&nbsp;фитнес-центра.</li>
+              <li>Чтобы сделать вашу мебель особенной и&nbsp;яркой, <span> мы&nbsp;можем окрасить продукцию в&nbsp;любые цвета </span> каталога RAL. Индивидуальные цветовые решения делают практичную металлическую мебель эстетичной и&nbsp;стильной.</li>
+              <li><span>Мебель может быть изготовлена по вашим чертежам</span>, с&nbsp;учётом параметров помещения и&nbsp;особенностей задач хранения; любых габаритов, цвета и&nbsp;комплектации. Сложные функциональные решения&nbsp;&mdash; наша специализация.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-xl d-none d-xl-block">
+          <div class="sMetalFurniture__content">
+            <div class="sMetalFurniture__vertical">METROMMEBEL
+            </div>
+            <ul>
+              <li class="empty"></li>
+              <li>Чтобы сделать вашу мебель особенной и&nbsp;яркой, <span> мы&nbsp;можем окрасить продукцию в&nbsp;любые цвета </span> каталога RAL. Индивидуальные цветовые решения делают практичную металлическую мебель эстетичной и&nbsp;стильной.</li>
+              <li><span>Мебель может быть изготовлена по вашим чертежам</span>, с&nbsp;учётом параметров помещения и&nbsp;особенностей задач хранения; любых габаритов, цвета и&nbsp;комплектации. Сложные функциональные решения&nbsp;&mdash; наша специализация.</li>
+            </ul><img class="img-1" src="img/svg/square.svg" alt="" loading="lazy"/><img class="img-2" src="img/svg/square.svg" alt="" loading="lazy"/><img class="img-3" src="img/svg/square.svg" alt="" loading="lazy"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end sMetalFurniture-->
+`
